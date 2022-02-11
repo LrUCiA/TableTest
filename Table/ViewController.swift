766956e9aaa3100e53b2f5b427122b7a0f500eba
LrUCiA <crosshertz7@gmail.com>
2022-02-11 21:06:19 +0900

    .

--- Table/ViewController.swift
+++ Table/ViewController.swift
@@ -7,13 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    //UITableViewDelegate는 필수 채택 함수가 없음.
+    @IBOutlet weak var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    //UITableViewDataSource 필수 채택 함수.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 
 }
 
