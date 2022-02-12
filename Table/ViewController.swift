8a0d21e5a7b2cf20d651e272930235f79afc5cab
LrUCiA <crosshertz7@gmail.com>
2022-02-12 18:02:36 +0900

    .

--- Table/ViewController.swift
+++ Table/ViewController.swift
@@ -7,23 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+class ViewController: UIViewController,UITableViewDelegate {
     //UITableViewDelegate는 필수 채택 함수가 없음.
     @IBOutlet weak var table: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        table.delegate = self
+        table.dataSource = self
         // Do any additional setup after loading the view.
     }
+   
+}
+
+extension ViewController: UITableViewDataSource {
     //UITableViewDataSource 필수 채택 함수.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        
+        let cell = UITableViewCell.init(style: .default, reuseIdentifier: "myCell")
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+        
+       // return UITableViewCell() //클래스 이름 뒤 '()'를 붙이면 인스턴스 생성
+    
     }
     
 
 }
-
